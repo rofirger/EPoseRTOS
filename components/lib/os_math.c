73de467ea8e4b1dc9c5203614080e99739168a81
Rofirger <842474373@qq.com>
2024-03-31 12:46:13 +0800

    add os_abs

--- components/lib/os_math.c
+++ components/lib/os_math.c
@@ -1,11 +1,18 @@
 
-float os_fabs(float fnum)
+#include "../../os_def.h"
+
+int os_abs(int n)
+{
+    return (n ^ (n >> 31)) - (n >> 31);
+}
+
+float os_fabsf(float fnum)
 {
     *((int *)&fnum) &= 0x7FFFFFFF;
     return fnum;
 }
 
-double os_abs(double dnum)
+double os_fabs(double dnum)
 {
     *(((int *)&dnum) + 1) &= 0x7FFFFFFF;
     return dnum;
@@ -13,11 +20,11 @@ double os_abs(double dnum)
 
 float os_sinf(float rad)
 {
-#define precision (1e-5)
+#define sinf_precision (1e-5)
     // Based on Taylor formula
     float sum = 0, item = rad;
     int k = 1;
-    while (os_fabs(item) > precision) {
+    while (os_fabsf(item) > sinf_precision) {
         sum += item;
         k += 1;
         item = (-1) * item * rad * rad / (2 * (k - 1) * (2 * k - 1));
@@ -27,14 +34,35 @@ float os_sinf(float rad)
 
 float os_cosf(float rad)
 {
-#define precision (1e-5)
+#define cosf_precision (1e-5)
     // Based on Taylor formula
     float sum = 0, item = 1;
     int k = 0;
-    while (os_fabs(item) > precision) {
+    while (os_fabsf(item) > cosf_precision) {
         sum += item;
         k += 1;
         item = (-1) * item * rad * rad / ((2 * k - 1) * 2 * k);
+    }
+    return sum;
+}
+
+float os_tanf(float rad)
+{
+    float c = os_cosf(rad);
+    OS_ASSERT(c != 0);
+    return os_sinf(rad) / c;
+}
+
+float os_atanf(float x)
+{
+#define atanf_precision (1e-5)
+    // Based on Taylor formula
+    float sum = 0, item = x;
+    int k = 0;
+    while (os_fabsf(item) > atanf_precision) {
+        sum += item;
+        k += 1;
+        item = (-1) * item * x * x * (2 * k - 1) / (2 * k + 1) ;
     }
     return sum;
 }
--- components/lib/os_math.h
+++ components/lib/os_math.h
@@ -2,8 +2,12 @@
 #ifndef _OS_MATH_H_
 #define _OS_MATH_H_
 
-float os_fabs(float fnum);
-double os_abs(double dnum);
+int os_abs(int n);
+float os_fabsf(float fnum);
+double os_fabs(double dnum);
 float os_sinf(float rad);
 float os_cosf(float rad);
+float os_tanf(float rad);
+float os_atanf(float x);
+
 #endif /* _OS_MATH_H_ */
